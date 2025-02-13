a879f63490f67402ec8b13c91c468be4fdfb6217
arbeoten <arbeoten@naver.com>
2025-02-13 17:35:07 +0900

    0213

--- models/studioCommunity.js
+++ models/studioCommunity.js
@@ -4,12 +4,6 @@ module.exports = class StudioCommunity extends Sequelize.Model {
    static init(sequelize) {
       return super.init(
          {
-            // 기본 키 (PK)
-            id: {
-               type: Sequelize.INTEGER,
-               primaryKey: true,
-               autoIncrement: true,
-            },
             // 제목
             title: {
                type: Sequelize.STRING(500),
