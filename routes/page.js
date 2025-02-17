cd7ca2a07dff4b8d3ca2bd799cf075a31dd152d0
psb6396 <psb6396@naver.com>
2025-02-17 17:37:23 +0900

    페이지 라우터 추가

---
+++ routes/page.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+const { isLoggedIn } = require('./middlewares')
+
+//프로필 조회 localhost:8000/page/profile
+router.get('/profile', isLoggedIn, async (req, res) => {
+   res.json({
+      success: true,
+      user: req.user,
+      message: '프로필 정보를 성공적으로 가져왔습니다.',
+   })
+})
+
+module.exports = router
